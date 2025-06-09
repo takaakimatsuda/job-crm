35f83c86f455cc86b0e61720959f3117b5216e61
matsudatakaaki <matsuda.t@sailboat.co.jp>
2025-06-09 11:30:22 +0900

    Companyモデルにファクトリを追加し、fillableプロパティを設定。webルートにCompanyControllerのリソースルートを追加。

--- src/app/Models/Company.php
+++ src/app/Models/Company.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Company extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'status',
+        'hope_level',
+        'memo',
+    ];
 }

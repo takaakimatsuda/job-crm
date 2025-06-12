59f408b06fb4e19512ef7c70c712da83e78a0a5e
matsudatakaaki <matsuda.t@sailboat.co.jp>
2025-06-12 15:49:44 +0900

    docker-compose.ymlとvite.config.jsにポート設定を追加

--- src/vite.config.js
+++ src/vite.config.js
@@ -17,4 +17,10 @@ export default defineConfig({
             },
         }),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        allowedHosts: ['host.docker.internal'],
+    },
 });

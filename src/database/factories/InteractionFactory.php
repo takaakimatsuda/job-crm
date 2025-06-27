caf518dbed37338729362f0dcbc391044f222922
matsudatakaaki <matsuda.t@sailboat.co.jp>
2025-06-27 12:03:11 +0900

    feat: InteractionFactoryのデフォルト状態を日本語のデータに変更
    InteractionFactory.phpの定義メソッドを修正し、Fakerを使用して日本語のインタラクションデータを生成するようにしました。具体的には、'memo'フィールドに日本語のランダムなメッセージを追加し、'summary'フィールドには40文字の日本語テキストを生成するように変更しました。

--- src/database/factories/InteractionFactory.php
+++ src/database/factories/InteractionFactory.php
@@ -19,11 +19,19 @@ class InteractionFactory extends Factory
      */
     public function definition(): array
     {
+        $faker = \Faker\Factory::create('ja_JP');
+
         return [
-            'interaction_date' => $this->faker->date(),
-            'type'             => $this->faker->randomElement(['電話', '面談', 'メール']),
-            'memo'             => $this->faker->paragraph(),
-            'summary'          => $this->faker->sentence(),
+            'interaction_date' => $faker->date(),
+            'type'             => $faker->randomElement(['電話', '面談', 'メール']),
+            'memo' => $faker->randomElement([
+                '本日、電話にて一次面談の調整を行った。',
+                '採用担当の○○氏と初回面談を実施。',
+                '書類選考を通過し、次回面接日を調整中。',
+                '内定の連絡あり。条件確認を進める予定。',
+                '残念ながら辞退の意向を伝えた。',
+            ]),
+            'summary'          => $faker->realText(40),
         ];
     }
 }
